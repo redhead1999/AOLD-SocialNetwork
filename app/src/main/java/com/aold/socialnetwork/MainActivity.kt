5f5fbfb75866ba7ca437b563f0540ebbbeb7ce04
Кирилин Юрий Александрович <kirilin.ya@avgd.pro>
2022-06-10 13:29:46 +0400

    AOLD-28 Класс для анимаций

--- app/src/main/java/com/aold/socialnetwork/MainActivity.kt
+++ app/src/main/java/com/aold/socialnetwork/MainActivity.kt
@@ -3,21 +3,29 @@ package com.aold.socialnetwork
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import coil.ImageLoader
 import coil.annotation.ExperimentalCoilApi
 import com.aold.socialnetwork.core.presentation.components.StandardScaffold
 import com.aold.socialnetwork.core.presentation.components.Navigation
 import com.aold.socialnetwork.presentation.ui.theme.SocialNetworkTheme
 import com.aold.socialnetwork.core.util.Screen
+import com.aold.socialnetwork.feature_activity.presentation.ActivityScreen
+import com.aold.socialnetwork.feature_post.presentation.main_feed.MainFeedScreen
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.DelicateCoroutinesApi
 import javax.inject.Inject
@@ -32,10 +40,12 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var imageLoader: coil.ImageLoader
 
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SocialNetworkTheme {
+
                 Surface(
                     color = MaterialTheme.colors.background,
                     modifier = Modifier.fillMaxSize()
