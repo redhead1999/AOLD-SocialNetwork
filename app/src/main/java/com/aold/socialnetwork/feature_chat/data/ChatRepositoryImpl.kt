49582bd3f8248390530c4c3e4a603fd8957830f2
Кирилин Юрий Александрович <kirilin.ya@avgd.pro>
2022-06-12 09:14:48 +0400

    Фикс вкладки чата

--- app/src/main/java/com/aold/socialnetwork/feature_chat/data/ChatRepositoryImpl.kt
+++ app/src/main/java/com/aold/socialnetwork/feature_chat/data/ChatRepositoryImpl.kt
@@ -1,20 +1,28 @@
 package com.aold.socialnetwork.feature_chat.data
 
+import com.aold.socialnetwork.R
 import com.aold.socialnetwork.core.util.Resource
+import com.aold.socialnetwork.core.util.UiText
 import com.aold.socialnetwork.feature_chat.data.remote.ChatApi
 import com.aold.socialnetwork.feature_chat.data.remote.ChatService
+import com.aold.socialnetwork.feature_chat.data.remote.dto.WsClientMessage
 import com.aold.socialnetwork.feature_chat.di.ScarletInstance
 import com.aold.socialnetwork.feature_chat.domain.models.Chat
 import com.aold.socialnetwork.feature_chat.domain.models.Message
 import com.aold.socialnetwork.feature_chat.domain.repository.ChatRepository
 import com.tinder.scarlet.WebSocket
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.receiveAsFlow
 import okhttp3.OkHttpClient
+import retrofit2.HttpException
+import java.io.IOException
 
 class ChatRepositoryImpl(
     private val chatApi: ChatApi,
     private val okHttpClient: OkHttpClient
-): ChatRepository {
+) : ChatRepository {
 
     private var chatService: ChatService? = null
 
@@ -23,26 +31,63 @@ class ChatRepositoryImpl(
     }
 
     override suspend fun getChatsForUser(): Resource<List<Chat>> {
-        TODO("Not yet implemented")
+        return try {
+            val chats = chatApi
+                .getChatsForUser()
+                .mapNotNull { it.toChat() }
+            Resource.Success(data = chats)
+        } catch (e: IOException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.error_couldnt_reach_server)
+            )
+        } catch (e: HttpException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.oops_something_went_wrong)
+            )
+        }
     }
+
 
     override suspend fun getMessagesForChat(
         chatId: String,
         page: Int,
         pageSize: Int
     ): Resource<List<Message>> {
-        TODO("Not yet implemented")
+        return try {
+            val messages = chatApi
+                .getMessagesForChat(chatId, page, pageSize)
+                .map { it.toMessage() }
+            Resource.Success(data = messages)
+        } catch (e: IOException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.error_couldnt_reach_server)
+            )
+        } catch (e: HttpException) {
+            Resource.Error(
+                uiText = UiText.StringResource(R.string.oops_something_went_wrong)
+            )
+        }
     }
 
     override fun observeChatEvents(): Flow<WebSocket.Event> {
-        TODO("Not yet implemented")
+        return chatService?.observeEvents()
+            ?.receiveAsFlow() ?: flow { }
     }
 
     override fun observeMessages(): Flow<Message> {
-        TODO("Not yet implemented")
+        return chatService
+            ?.observeMessages()
+            ?.receiveAsFlow()
+            ?.map { it.toMessage() } ?: flow { }
     }
 
     override fun sendMessage(toId: String, text: String, chatId: String?) {
-        TODO("Not yet implemented")
+        chatService?.sendMessage(
+            WsClientMessage(
+                toId = toId,
+                text = text,
+                chatId = chatId
+            )
+        )
     }
 }
